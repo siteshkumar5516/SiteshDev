df2f58b022e933803669ecfc5f33ad44ceab1ed0
sitesh <sitesh@cloudthing.com>
2020-01-21 13:27:47 +0530

    tested

---
+++ TestB2c/Common/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+
+namespace Common
+{
+    public static class Helper
+    {
+        public static string TenantId
+        {
+
+            get
+
+            {
+                var appsettingValue = Environment.GetEnvironmentVariable("b2c:Tenant");
+                return !string.IsNullOrEmpty(appsettingValue) ? appsettingValue : ConfigurationManager.AppSettings.Get("b2c:Tenant");
+            }
+        }
+        public static string ClientId
+        {
+            get
+
+            {
+                var appsettingValue = Environment.GetEnvironmentVariable("b2c:ClientId");
+                return !string.IsNullOrEmpty(appsettingValue) ? appsettingValue : ConfigurationManager.AppSettings.Get("b2c:ClientId");
+            }
+        }
+        public static string ClientSecret
+        {
+            get
+
+            {
+                var appsettingValue = Environment.GetEnvironmentVariable("b2c:ClientSecret");
+                return !string.IsNullOrEmpty(appsettingValue) ? appsettingValue : ConfigurationManager.AppSettings.Get("b2c:ClientSecret");
+            }
+        }
+        public static string KeyVault
+        {
+            get
+
+            {
+                var appsettingValue = Environment.GetEnvironmentVariable("Keyvault");
+                return !string.IsNullOrEmpty(appsettingValue) ? appsettingValue : ConfigurationManager.AppSettings.Get("Keyvault");
+            }
+        }
+    }
+}
